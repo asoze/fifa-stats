4addd01d2bc23bf42d1ab6a4a997cef58613c1ea
andrewsweeney <andrew.sweeney@turner.com>
2019-12-02 22:00:38 -0500

    master: Displays match results, not much else

--- src/components/MatchTable.js
+++ src/components/MatchTable.js
@@ -2,6 +2,14 @@ import React from 'react';
 import {fetches} from '../data/fetches';
 
 class MatchTable extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            matches: []
+        }
+    }
 
     getTableHeaders() {
         return (
@@ -18,35 +26,91 @@ class MatchTable extends React.Component {
     }
 
     getMatches() {
-        fetches.getMatches().then( (eachDataset) => {
-            console.log("EACH DA", eachDataset.data);
+        if ( this.state.matches && this.state.matches.length > 0 ) {
+            console.log("GOOD!");
+            return this.state.matches;
+        }
+        
+        const matches = fetches.getMatches();
+        const teams = fetches.getTeams();
+        
+        Promise.all( [matches, teams] )
+            .then( (values) => {
+                const matches = values[0].data;
+                const teams = values[1].data;
+                
+                const fullMatches = matches.map( (eachMatch) => {
+                    eachMatch.team1 = teams[ eachMatch.team1_id-1 ];
+                    eachMatch.team2 = teams[ eachMatch.team2_id-1 ];
+                    
+                    return eachMatch;
+                });
+                
+                console.log("MAtches rEady", fullMatches);
+                
+                this.setState({
+                    matches: fullMatches
+                });
+            })
 
-            // return matchCollection.map( (eachMatch, idx) => {
-            //     return this.formatMatchRow( eachMatch, idx );
-            // })
-        });
-
+        // const matches = fetches.getMatches()
+        //     .then( (matches) => {
+        //         fetches.getTeams().then( (teams) => {
+        // 
+        // 
+        // 
+        // 
+        // 
+        //             const fullMatchData = matches.data;
+        //             const teamData = teams.data;
+        //             console.log("TEAM 1", fullMatchData.team1_id);
+        // 
+        //             fullMatchData.home_team = teamData[fullMatchData.team1_id];
+        //             fullMatchData.away_team = teamData[fullMatchData.team2_id];
+        // 
+        //             console.log("DULL@2", fullMatchData);
+        //             return fullMatchData;
+        //         });
+        // 
+        //         // return matchCollection.map( (eachMatch, idx) => {
+        //         //     return this.formatMatchRow( eachMatch, idx );
+        //         // })
+        //     });
     }
 
     formatMatchRow( match, idx ) {
         const keyString = `match_${idx}`;
+        console.log("MATCH", match);
         return (
             <tr key={keyString}>
                 <td>{ match.round_id }</td>
-                <td>{ match.home_team }</td>
-                <td>{ match.away_team }</td>
-                <td>{ match.score }</td>
-                <td>{ match.match_time }</td>
+                <td>{ match.team1.title }</td>
+                <td>{ match.team2.title }</td>
+                <td>{ `${ match.score1 } - ${ match.score2 }` }</td>
+                    <td>{ match.play_at }</td>
             </tr>
         )
     }
 
     getTableBody() {
+        const matches = this.getMatches();
+        if ( matches ) {
+            const renderedRows = matches.map( (eachMatch, idx) => {
+                return this.formatMatchRow( eachMatch, idx );
+            })
+            
+            return (
+                <tbody>
+                    { renderedRows }
+                </tbody>
+            )
+        };
+        
         return (
             <tbody>
-                { this.getMatches() }
+                <tr><td colSpan='5'>Loading...</td></tr>
             </tbody>
-        )
+        );
     }
 
     render() {
--- src/data/fetches.js
+++ src/data/fetches.js
@@ -6,10 +6,10 @@ const fetches = {
             "method":"GET",
             "url":"https://montanaflynn-fifa-world-cup.p.rapidapi.com/games",
             "headers":{
-            "content-type":"application/octet-stream",
-            "x-rapidapi-host":"montanaflynn-fifa-world-cup.p.rapidapi.com",
-            "x-rapidapi-key":"40e59020e0mshc0803894d97393bp103acbjsn351b37664b6f",
-            "accepts":"json"
+                "content-type":"application/octet-stream",
+                "x-rapidapi-host":"montanaflynn-fifa-world-cup.p.rapidapi.com",
+                "x-rapidapi-key":"40e59020e0mshc0803894d97393bp103acbjsn351b37664b6f",
+                "accepts":"json"
         }
         });
     },
@@ -18,10 +18,10 @@ const fetches = {
             "method":"GET",
             "url":"https://montanaflynn-fifa-world-cup.p.rapidapi.com/teams",
             "headers":{
-            "content-type":"application/octet-stream",
-            "x-rapidapi-host":"montanaflynn-fifa-world-cup.p.rapidapi.com",
-            "x-rapidapi-key":"40e59020e0mshc0803894d97393bp103acbjsn351b37664b6f",
-            "accepts":"json"
+                "content-type":"application/octet-stream",
+                "x-rapidapi-host":"montanaflynn-fifa-world-cup.p.rapidapi.com",
+                "x-rapidapi-key":"40e59020e0mshc0803894d97393bp103acbjsn351b37664b6f",
+                "accepts":"json"
             }
         });
     },
@@ -30,10 +30,34 @@ const fetches = {
             "method":"GET",
             "url":"https://montanaflynn-fifa-world-cup.p.rapidapi.com/persons",
             "headers":{
-            "content-type":"application/octet-stream",
-            "x-rapidapi-host":"montanaflynn-fifa-world-cup.p.rapidapi.com",
-            "x-rapidapi-key":"40e59020e0mshc0803894d97393bp103acbjsn351b37664b6f",
-            "accepts":"json"
+                "content-type":"application/octet-stream",
+                "x-rapidapi-host":"montanaflynn-fifa-world-cup.p.rapidapi.com",
+                "x-rapidapi-key":"40e59020e0mshc0803894d97393bp103acbjsn351b37664b6f",
+                "accepts":"json"
+            }
+        });
+    },
+    getRounds: function() {
+        return axios({
+            "method":"GET",
+            "url":"https://montanaflynn-fifa-world-cup.p.rapidapi.com/rounds",
+            "headers":{
+                "content-type":"application/octet-stream",
+                "x-rapidapi-host":"montanaflynn-fifa-world-cup.p.rapidapi.com",
+                "x-rapidapi-key":"40e59020e0mshc0803894d97393bp103acbjsn351b37664b6f",
+                "accepts":"json"
+            }
+        });
+    },
+    getGoals: function() {
+        return axios({
+            "method":"GET",
+            "url":"https://montanaflynn-fifa-world-cup.p.rapidapi.com/goals",
+            "headers":{
+                "content-type":"application/octet-stream",
+                "x-rapidapi-host":"montanaflynn-fifa-world-cup.p.rapidapi.com",
+                "x-rapidapi-key":"40e59020e0mshc0803894d97393bp103acbjsn351b37664b6f",
+                "accepts":"json"
             }
         });
     }
